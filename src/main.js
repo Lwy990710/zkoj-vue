5e4ce04d7e7405c33a351ab0b9c50076734832c6
snowmeow <yuki754685421@163.com>
2020-11-09 19:59:52 +0800

    修复axios钩子函数不触发的bug

--- src/main.js
+++ src/main.js
@@ -11,17 +11,20 @@ Vue.config.productionTip = false
 //Vue.prototype.base_url = "http://yapi.yukineko.top/mock/16/zkoj/"
 Vue.prototype.base_url = "http://localhost:8080"
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
-
-
 axios.interceptors.request.use(config => {
   // 为请求头对象，添加 Token 验证的 Authorization 字段
+  console.log("axios: " + store.state.is_login);
   if (store.state.is_login)
     config.headers['Authorization'] = store.state.token;
   //必须return config 这是固定写法
   return config
-})
+});
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app');
+
+
+
