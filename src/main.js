042cff7c5628a9d26beff89b51e9ba9497820754
snowmeow <yuki754685421@163.com>
2020-11-16 00:45:15 +0800

    [修复] 修复了提交评测的编辑器无法正确地切换语言的Bug（切换语言时并非改变当前编辑器的高亮显示格式而是错误地再创建了一个新的编辑器窗口，导致编辑器窗口越来越多）。

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import "codemirror/addon/hint/show-hint.css";
 Vue.config.productionTip = false
 //Vue.prototype.base_url = "http://yapi.yukineko.top/mock/16/zkoj/"
 Vue.prototype.base_url = "http://localhost:8080"
-
 axios.interceptors.request.use(config => {
   // 为请求头对象，添加 Token 验证的 Authorization 字段
   console.log("axios: " + store.state.is_login);
--- src/views/AnswerPage.vue
+++ src/views/AnswerPage.vue
@@ -1,183 +1,177 @@
 <template>
-    <div class="main" >
-        <div class="answer_main">
-            <div class="back">
-                <el-button type="primary" @click="back">返回题目</el-button>
-            </div>
-            <!-- 问题描述 -->
-            <div class="answer_problem_description">
-                <h2>问题描述</h2>
-                <div class="answer_problem_content" id="answer_problem_description_content">
-                    这里是问题描述
-                </div>
-            </div>
+  <div class="main">
+    <div class="answer_main">
+      <div class="back">
+        <el-button type="primary" @click="back">返回题目</el-button>
+      </div>
+      <!-- 问题描述 -->
+      <div class="answer_problem_description">
+        <h2>问题描述</h2>
+        <div class="answer_problem_content" id="answer_problem_description_content">
+          这里是问题描述
         </div>
-
-        <div class="language_choose">
-            <el-select v-model="value" placeholder="请选择语言" @change="change">
-                <el-option v-for="language in options"
-                           :key="language.value"
-                           :label="language.label"
-                           :value="language.value">
-                </el-option>
-            </el-select>
-        </div>
-
-        <div class="test">
-            <textarea ref="mycode"  style="height:200px;width:600px;"></textarea>
-        </div>
-
-        <div class="commit">
-            <el-button type="primary">提交答案</el-button>
-        </div>
+      </div>
     </div>
+
+    <div class="language_choose">
+      <el-select v-model="value" placeholder="请选择语言" @change="change">
+        <el-option v-for="language in options"
+                   :key="language.value"
+                   :label="language.label"
+                   :value="language.value">
+        </el-option>
+      </el-select>
+    </div>
+
+    <div class="test">
+      <textarea id="input_area" ref="mycode" style="height:200px;width:600px;"></textarea>
+    </div>
+
+    <div class="commit">
+      <el-button type="primary" @click="submit">提交答案</el-button>
+    </div>
+  </div>
 </template>
 
 
 <script>
-  import "codemirror/theme/ambiance.css";
-  import "codemirror/lib/codemirror.css";
-  import "codemirror/addon/hint/show-hint.css";
-  let CodeMirror = require("codemirror/lib/codemirror");
-  // require("codemirror/addon/edit/matchbrackets");
-  // require("codemirror/addon/selection/active-line");
-  // require ("codemirror/mode/clike/clike");
-  // require("codemirror/mode/sql/sql");
-  // require("codemirror/addon/hint/sql-hint");
-  import 'codemirror/mode/javascript/javascript'
-  import 'codemirror/mode/clike/clike'
-  import 'codemirror/mode/go/go'
-  import 'codemirror/mode/htmlmixed/htmlmixed'
-  import 'codemirror/mode/http/http'
-  import 'codemirror/mode/php/php'
-  import 'codemirror/mode/python/python'
-  import 'codemirror/mode/http/http'
-  import 'codemirror/mode/sql/sql'
-  import 'codemirror/mode/vue/vue'
-  import 'codemirror/mode/xml/xml'
+import "codemirror/theme/ambiance.css";
+import "codemirror/lib/codemirror.css";
+import "codemirror/addon/hint/show-hint.css";
 
-    export default {
-        name: "AnswerPage",
-        data(){
-            return{
-                options:[{
-                    value: 'Java',
-                    label: 'Java',
-                },{
-                    value: 'Python',
-                    label: 'Python',
-                },{
-                    value: 'c++',
-                    label: 'c++'
-                }],
-                value: ''
-            }
-        },
-        mounted () {
-            debugger
-            let editor = CodeMirror.fromTextArea(this.$refs.mycode, {
-                mode: 'text/x-java',
-                indentWithTabs: true,
-                smartIndent: true,
-                lineNumbers: true,
-                matchBrackets: true,
+let CodeMirror = require("codemirror/lib/codemirror");
+// require("codemirror/addon/edit/matchbrackets");
+// require("codemirror/addon/selection/active-line");
+// require ("codemirror/mode/clike/clike");
+// require("codemirror/mode/sql/sql");
+// require("codemirror/addon/hint/sql-hint");
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/clike/clike'
+import 'codemirror/mode/go/go'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/mode/http/http'
+import 'codemirror/mode/php/php'
+import 'codemirror/mode/python/python'
+import 'codemirror/mode/http/http'
+import 'codemirror/mode/sql/sql'
+import 'codemirror/mode/vue/vue'
+import 'codemirror/mode/xml/xml'
 
-                // extraKeys: {'Ctrl': 'autocomplete'},自定义快捷键
-                // hintOptions: {自定义提示选项
-                //     tables: {
-                //         users: ['name', 'score', 'birthDate'],
-                //         countries: ['name', 'population', 'size']
-                //     }
-                // }
-            })
-            //代码自动提示功能，记住使用cursorActivity事件不要使用change事件，这是一个坑，那样页面直接会卡死
-            editor.on('cursorActivity', function () {
-                editor.showHint()
-            })
-        },
-        beforeRouteEnter(to,from,next){
-            window.document.body.style.backgroundColor = '#EFEFEF';
-            next();
-        },
-        methods: {
-            back() {
-                this.$router.go(-1);
-            },
-            change(){
-                debugger
-                let mime = 'text/x-java'
-                if(this.value === 'Java'){
-                    mime = 'text/x-java'
-                } else if(this.value === 'Python'){
-                    mime = 'text/x-python'
-                } else if(this.value === 'c++'){
-                    mime = 'text/x-c++src'
-                }
-                // alert(mime);
-                let editor = CodeMirror.fromTextArea(this.$refs.mycode, {
-                    mode: mime,
-                    indentWithTabs: true,
-                    smartIndent: true,
-                    lineNumbers: true,
-                    matchBrackets: true,
+export default {
 
-                    extraKeys: {'Ctrl': 'autocomplete'},
-                    hintOptions: {
-                        tables: {
-                            users: ['name', 'score', 'birthDate'],
-                            countries: ['name', 'population', 'size']
-                        }
-                    }
-                })
-                // 代码自动提示功能，记住使用cursorActivity事件不要使用change事件，这是一个坑，那样页面直接会卡死
-                editor.on('cursorActivity', function () {
-                    editor.showHint()
-                })
-            }
-        }
+  name: "AnswerPage",
+  data() {
+    return {
+      options: [{
+        value: 'Java',
+        label: 'Java',
+      }, {
+        value: 'Python',
+        label: 'Python',
+      }, {
+        value: 'c++',
+        label: 'c++'
+      }],
+      value: '',
     }
+  },
+  mounted() {
+
+    let editor = CodeMirror.fromTextArea(document.getElementById("input_area"), {
+      mode: 'text/x-java',
+      indentWithTabs: true,
+      smartIndent: true,
+      lineNumbers: true,
+      matchBrackets: true,
+/*      extraKeys: {'Ctrl': 'autocomplete'},自定义快捷键
+      hintOptions: {自定义提示选项
+          tables: {
+              users: ['name', 'score', 'birthDate'],
+              countries: ['name', 'population', 'size']
+          }
+      }*/
+    })
+    //代码自动提示功能，记住使用cursorActivity事件不要使用change事件，这是一个坑，那样页面直接会卡死
+    /*editor.on('cursorActivity', function () {
+      editor.showHint()
+    });*/
+
+    this.editor = editor;
+  },
+  beforeRouteEnter(to, from, next) {
+    window.document.body.style.backgroundColor = '#EFEFEF';
+    next();
+  },
+  methods: {
+    back() {
+      this.$router.go(-1);
+    },
+    change() {
+
+      let mime = 'text/x-java'
+      if (this.value === 'Java') {
+        mime = 'text/x-java'
+      } else if (this.value === 'Python') {
+        mime = 'text/x-python'
+      } else if (this.value === 'c++') {
+        mime = 'text/x-c++src'
+      }
+
+      this.editor.setOption("mode", mime);
+
+    },
+    submit() {
+    // TODO : 提交答案
+      let editor = this.editor;
+      let source_code = editor.getValue();
+
+
+
+    }
+  }
+}
 </script>
 
 <style scoped>
 
-    .main {
-        background-color: #ffffff!important;
-        margin: 0 auto;
-        max-width: 800px;
-        min-width: 800px;
-        box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
-    }
+.main {
+  background-color: #ffffff !important;
+  margin: 0 auto;
+  max-width: 800px;
+  min-width: 800px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+}
 
-    .answer_main{
-        width: 600px;
-        margin: 0 auto;
-    }
+.answer_main {
+  width: 600px;
+  margin: 0 auto;
+}
 
-    .back{
-        padding-top: 20px;
-        text-align: center;
-    }
+.back {
+  padding-top: 20px;
+  text-align: center;
+}
 
-    .answer_problem_content{
-        text-indent: 2em;
-    }
+.answer_problem_content {
+  text-indent: 2em;
+}
 
-    .language_choose{
-        margin: 20px auto;
-        width: 600px;
-    }
+.language_choose {
+  margin: 20px auto;
+  width: 600px;
+}
 
-    .test{
-        border: solid 1px black;
-        margin: 20px auto;
-        width: 600px;
-        font-size: 11pt;
-        font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
-    }
+.test {
+  border: solid 1px black;
+  margin: 20px auto;
+  width: 600px;
+  font-size: 11pt;
+  font-family: Consolas, Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+}
 
-    .commit{
-        padding-top: 20px;
-        text-align: center;
-        padding-bottom: 20px;
-    }
+.commit {
+  padding-top: 20px;
+  text-align: center;
+  padding-bottom: 20px;
+}
 </style>
