8fd8aba8572fa0e8a4cf15fc525c5ee81557ba56
snowmeow <yuki754685421@163.com>
2021-01-03 18:57:57 +0800

    [修改] 表格宽度优化

--- src/views/admin/ProblemManage.vue
+++ src/views/admin/ProblemManage.vue
@@ -81,18 +81,19 @@
           prop="id"
           label="题号"
           align="center"
-          width="90">
+          width="80">
         </el-table-column>
         <el-table-column
           prop="title"
           label="题名"
-          align="center"
-          width="200">
+          min-width="150"
+          show-overflow-tooltip="true">
         </el-table-column>
         <el-table-column
           prop="tag"
           label="算法标签"
-          width="320">
+          min-width="200"
+          show-overflow-tooltip="true">
           <template slot-scope="scope">
             <el-tag type="danger" effect="dark" size="mini" v-for="(item, index) in scope.row.tag" :key="index" style="margin: 0 5px">
               {{item.name}}
@@ -101,9 +102,10 @@
         </el-table-column>
         <el-table-column
           prop="problem_class.name"
-          width="80"
+          min-width="100"
           align="center"
-          label="分组">
+          label="分组"
+          show-overflow-tooltip="true">
         </el-table-column>
         <el-table-column
           prop="difficulty"
@@ -132,7 +134,7 @@
         </el-table-column>
         <el-table-column
           sortable
-          width="90"
+          width="100"
           align="center"
           label="通过率">
           <template slot-scope="scope">
