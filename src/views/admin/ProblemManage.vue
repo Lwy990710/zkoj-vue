a8545ab0b4e2ba553d9e14ed5797f57d81b6b275
lwy <294331944@qq.com>
2021-01-04 13:13:37 +0800

    管理问题页面更新

--- src/views/admin/ProblemManage.vue
+++ src/views/admin/ProblemManage.vue
@@ -1,29 +1,21 @@
 <template>
-  <div class="main">
+  <div class="main clear">
     <!-- TODO: 筛选条件区域 -->
-    <div class="problem_screen">
-      <div class="add_problem">
-        <el-button size="small" style="display: inline-block; margin-right: 10px">
-        <router-link style="color: black" :to="'add'">增加题目</router-link>
-        </el-button>
-        <el-button size="small" style="display: inline-block; margin: 0 10px">删除题目</el-button>
-        <el-button size="small" style="display: inline-block; margin: 0 10px">增加标签</el-button>
-        <el-button size="small" style="display: inline-block; margin: 0 10px">增加分类</el-button>
-        <el-divider></el-divider>
-      </div>
+    <div class="problem_screen clear">
+      <!-- 搜索问题区域 -->
       <div class="search_area">
-
+        <!-- 题目搜索框 -->
         <div class="search_problem">
           <p>搜索题目:</p>
-          <el-input size="small" style="width: 400px" id="search_problem_input" v-model="input_search_problem" placeholder="输入题号或题目名搜索"></el-input>
+          <el-input @change="searchProblemIdOrTitle" size="small" style="width: 300px" id="search_problem_input" v-model="input_search_problem" placeholder="输入题号或题目名搜索"></el-input>
           <el-button @click="searchProblemIdOrTitle" size="small" type="primary" style="display: inline-block;margin: 0 10px;width: 80px">搜索</el-button>
           <el-divider></el-divider>
         </div>
-
+        <!-- 条件筛选 -->
         <div class="condition_screen">
           <p>根据条件搜索题目:</p>
           <span>难度</span>
-          <el-select size="small" v-model="value_difficulty" style="width: 200px;margin-right: 300px" @change="changeDifficulty">
+          <el-select size="small" v-model="value_difficulty" style="width: 200px;margin-right: 54px" @change="changeDifficulty">
             <el-option
                     v-for="item in options_difficulty"
                     :key="item.value_difficulty"
@@ -34,6 +26,11 @@
           <span>分组</span>
           <el-select size="small"  v-model="value_class" style="width: 200px" @change="changeClass">
             <el-option
+                    :key="null"
+                    label="全部分组"
+                    :value="null">
+            </el-option>
+            <el-option
                     v-for="item in class_list"
                     :key="item.id"
                     :label="item.name"
@@ -42,7 +39,12 @@
           </el-select>
           <br><br>
           <span>算法</span>
-          <el-select size="small"  v-model="value_algorithm" style="width: 200px;margin-right: 290px" @change="changeAlgorithm">
+          <el-select size="small"  v-model="value_algorithm" style="width: 200px;margin-right: 40px" @change="changeAlgorithm">
+            <el-option
+                    :key="null"
+                    label="全部算法"
+                    :value="null">
+            </el-option>
             <el-option
                     v-for="item in algorithm_list"
                     :key="item.id"
@@ -53,7 +55,8 @@
           <span>上传者</span>
           <el-input size="small"
             style="width: 200px"
-            d="search_up_username"
+            id="search_up_username"
+            @change="searchUsername"
             v-model="input_up_username"
             placeholder="输入用户名"></el-input>
           <el-button
@@ -67,8 +70,104 @@
       </div>
     </div>
 
+    <!-- 分组和算法标签管理区 -->
+    <div class="manage_area clear">
+      <el-tabs v-model="active_name" type="card" >
+        <!-- 算法管理页面 -->
+        <el-tab-pane label="算法标签管理" name="algorithm">
+          <div>
+            <div v-for="item in algorithm_list" style="margin: 20px">
+              <span style="width: 100px">{{item.name}}</span>
+              <el-button @click="modify_algorithm_dialog=true" style="width: 60px;display: inline-block" size="mini">修改</el-button>
+              <el-button @click="delAlgorithm(item)" style="width: 60px;display: inline-block" size="mini" type="danger">删除</el-button>
+            </div>
+            <!-- 修改算法对话框 -->
+            <el-dialog title="修改算法" :visible.sync="modify_algorithm_dialog" style="width: 1000px;margin: 0 auto">
+              <span>新的算法名 : </span>
+              <el-input v-model="modify_algorithm_name" size="small" placeholder="新算法名" style="width: 300px"></el-input>
+              <div slot="footer" class="dialog-footer">
+                <el-button size="small" @click="modify_algorithm_dialog = false">取 消</el-button>
+                <el-button size="small"  @click="modifyAlgorithm()" type="primary" >确 定</el-button>
+              </div>
+            </el-dialog>
+          </div>
+          <!-- 新增算法按钮 -->
+          <div style="text-align: center">
+            <el-button @click="add_algorithm_dialog = true" type="primary" size="small">增加算法标签</el-button>
+          </div>
+          <!-- 增加算法对话框 -->
+          <el-dialog title="增加算法" :visible.sync="add_algorithm_dialog" style="width: 1000px;margin: 0 auto">
+            <span>新算法标签 : </span>
+            <el-input v-model="new_algorithm_name" size="small" placeholder="算法名字" style="width: 300px"></el-input>
+            <div slot="footer" class="dialog-footer">
+              <el-button size="small" @click="add_algorithm_dialog = false">取 消</el-button>
+              <el-button size="small"  @click="addAlgorithm()" type="primary" >确 定</el-button>
+            </div>
+          </el-dialog>
+        </el-tab-pane>
+        <!-- 分组管理页面 -->
+        <el-tab-pane label="分组管理" name="class">
+          <div v-for="item in class_list" style="margin: 20px">
+            <span style="width: 100px">{{item.name}}</span>
+            <el-button @click="modify_class_dialog=true" style="width: 60px;display: inline-block" size="mini">修改</el-button>
+            <el-button @click="delClass(item)" style="width: 60px;display: inline-block" size="mini" type="danger">删除</el-button>
+          </div>
+          <!-- 修改分组对话框 -->
+          <el-dialog title="修改分组" :visible.sync="modify_class_dialog" style="width: 1000px;margin: 0 auto">
+            <span>新的分组名 : </span>
+            <el-input type="text" maxlength="40" show-word-limit v-model="modify_class_name" size="small" placeholder="分组名字" style="width: 300px"></el-input>
+            <br><br>
+            <span>修改后的分组描述 : </span>
+            <div style="margin: 10px 28px">
+              <el-input
+                      type="textarea"
+                      maxlength="255"
+                      show-word-limit
+                      resize="none"
+                      :autosize="{minRows: 6, maxRows: 6}"
+                      v-model="modify_class_describe"
+                      size="small" placeholder="分组描述"
+                      style="width: 400px;font-size: 16px">
+              </el-input>
+            </div>
+            <div slot="footer" class="dialog-footer">
+              <el-button size="small" @click="add_class_dialog = false">取 消</el-button>
+              <el-button size="small"  @click="addAlgorithm()" type="primary" >确 定</el-button>
+            </div>
+          </el-dialog>
+          <!-- 新增分组按钮 -->
+          <div style="text-align: center">
+            <el-button @click="add_class_dialog = true" type="primary" size="small">增加分组</el-button>
+          </div>
+          <!-- 新增分组对话框 -->
+          <el-dialog title="增加分组" :visible.sync="add_class_dialog" style="width: 1000px;margin: 0 auto">
+            <span>新分组名字 : </span>
+            <el-input type="text" maxlength="40" show-word-limit v-model="new_class_name" size="small" placeholder="分组名字" style="width: 300px"></el-input>
+            <br><br>
+            <span>新分组描述 : </span>
+            <div style="margin: 10px 28px">
+              <el-input
+                type="textarea"
+                maxlength="255"
+                show-word-limit
+                resize="none"
+                :autosize="{minRows: 6, maxRows: 6}"
+                v-model="new_class_describe"
+                size="small" placeholder="分组描述"
+                style="width: 400px;font-size: 16px">
+              </el-input>
+            </div>
+            <div slot="footer" class="dialog-footer">
+              <el-button size="small" @click="add_class_dialog = false">取 消</el-button>
+              <el-button size="small"  @click="addAlgorithm()" type="primary" >确 定</el-button>
+            </div>
+          </el-dialog>
+        </el-tab-pane>
+      </el-tabs>
+    </div>
+
     <!-- 表格区域 -->
-    <div class="table_area">
+    <div class="table_area clear">
       <el-table
         v-loading="is_loading_table"
         :data="problem_list"
@@ -157,66 +256,6 @@
           </template>
         </el-table-column>
       </el-table>
-<!--      <el-table-->
-<!--        :data="problem_list"-->
-<!--        style="width: 100%">-->
-<!--        <el-table-column-->
-<!--          prop="id"-->
-<!--          label="题号"-->
-<!--          width="80px"-->
-<!--          align="center"-->
-<!--          min-width="30px"/>-->
-<!--        <el-table-column-->
-<!--          prop="title"-->
-<!--          label="题名"-->
-<!--          min-width="150px"-->
-<!--          show-overflow-tooltip="true"/>-->
-<!--        <el-table-column-->
-<!--          prop="problem_class.name"-->
-<!--          label="分组"-->
-<!--          min-width="100px"-->
-<!--          align="center"-->
-<!--          show-overflow-tooltip="true"/>-->
-<!--        <el-table-column-->
-<!--            prop="difficulty"-->
-<!--            label="难度"-->
-<!--            width="80px"-->
-<!--            align="center"-->
-<!--            show-overflow-tooltip="true"/>-->
-<!--        <el-table-column-->
-<!--          prop="count"-->
-<!--          label="提交数"-->
-<!--          sortable="custom"-->
-<!--          width="100px"-->
-<!--          align="center"/>-->
-<!--        <el-table-column-->
-<!--          prop="accepted"-->
-<!--          label="AC数"-->
-<!--          sortable="custom"-->
-<!--          width="100px"-->
-<!--          align="center"/>-->
-<!--        <el-table-column-->
-<!--          label="通过率"-->
-<!--          sortable="custom"-->
-<!--          width="100px"-->
-<!--          align="center">-->
-<!--          <template slot-scope="scope">-->
-<!--            <div v-if="scope.row.count === 0">-</div>-->
-<!--            <div v-else>{{(scope.row.accepted / scope.row.count * 100).toFixed(1)}}%</div>-->
-<!--          </template>-->
-<!--        </el-table-column>-->
-<!--        <el-table-column-->
-<!--                label="公开"-->
-<!--                prop="is_private"-->
-<!--                custom-->
-<!--                width="80px"-->
-<!--                align="center">-->
-<!--          <template slot-scope="scope">-->
-<!--            <div v-if="!is_private">是</div>-->
-<!--            <div v-if="is_private">否</div>-->
-<!--          </template>-->
-<!--        </el-table-column>-->
-<!--      </el-table>-->
     </div>
   </div>
 </template>
@@ -226,10 +265,35 @@ export default {
   name: "ProblemManage",
   data() {
     return {
-      input_search_problem: '',
-      input_up_username: '',
+      /* 加载界面 */
       is_loading_table: false,
+      /* 题目搜索框内容 */
+      input_search_problem: '',
+      /* 用户搜索框内容 */
+      input_up_username: '',
+      /* 分组标签管理页面 */
+      active_name: 'algorithm',
+      /* 增加算法对话框 */
+      add_algorithm_dialog: false,
+      new_algorithm_name: '',
+      /* 修改算法对话框 */
+      modify_algorithm_dialog: false,
+      modify_algorithm_id: null,
+      modify_algorithm_name: '',
+      /* 增加分组对话框 */
+      add_class_dialog: false,
+      new_class_name: '',
+      new_class_describe: '',
+      /* 修改分组对话框 */
+      modify_class_dialog: false,
+      modify_class_id: null,
+      modify_class_name: '',
+      modify_class_describe: '',
+
       options_difficulty: [{
+        value_difficulty: null,
+        label: '全部难度'
+      },{
         value_difficulty: '1',
         label: '简单'
       }, {
@@ -332,6 +396,58 @@ export default {
         this.is_loading_table = false;
       })
     },
+    /* 增加算法标签 */
+    addAlgorithm(){
+
+    },
+    /* 删除算法标签 */
+    delAlgorithm(item){
+      this.$confirm('是否确定删除算法 : ' + item.name, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+    /* 修改算法标签 */
+
+    /* 增加分组 */
+    addClass(){
+
+    },
+
+    /* 删除分组 */
+    delClass(item){
+      this.$confirm('是否确定删除分组 : ' + item.name, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
+    },
+
+    /* 修改分组 */
+    modifyClass(){
+
+    },
 
     /* 难度筛选 */
     changeDifficulty(val){
@@ -346,10 +462,12 @@ export default {
     },
     /* 算法筛选 */
     changeAlgorithm(val){
+      alert(val);
       this.value_algorithm = val;
       this.request_query.tag_id = val;
       this.request_problem_list();
     },
+
     /* 题号或题目筛选 */
     searchProblemIdOrTitle(){
       this.request_query.search = this.input_search_problem;
@@ -360,7 +478,7 @@ export default {
       this.request_query.upload_username = this.input_up_username;
       this.request_query.upload_user_id = this.input_up_username;
       this.request_problem_list();
-    }
+    },
   }
 }
 </script>
@@ -381,15 +499,43 @@ p{
  font-size: 14px;
 }
 
+.clear::before,
+.clear::after {
+  content: '';
+  display: table;
+  clear: both;
+}
 
-.problem_screen{
+.main{
+
+}
+
+.problem_screen {
+  min-width: 780px;
+  width: 780px;
+  float: left;
+  height: 260px;
+  margin-bottom: 20px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
   background-color: white;
   padding: 20px;
+
 }
 
-.table_area{
-  margin-top: 20px;
+.manage_area {
+  min-width: 350px;
+  float: right;
+  width: 400px;
+  height: 260px;
+  background-color: white;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04);
+  padding: 20px;
+  overflow:auto;
+
+}
+
+.table_area {
+  clear: both;
   background-color: white;
   box-shadow: 0 2px 4px rgba(0, 0, 0, .12), 0 0 6px rgba(0, 0, 0, .04)
 }
